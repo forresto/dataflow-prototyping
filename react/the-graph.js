a2ee2f1d2272951078e09275bcf09f0d2327dae0
Forrest Oliphant <forrest@sembiki.com>
2014-01-20 11:12:33 +0200

    fit and center

--- react/the-graph.js
+++ react/the-graph.js
@@ -136,12 +136,22 @@
     limits.maxX += TheGraph.nodeSize * 2;
     limits.maxY += TheGraph.nodeSize * 2;
 
-    var scaleX = width / (limits.maxX - limits.minX);
-    var scaleY = height / (limits.maxY - limits.minY);
+    var gWidth = limits.maxX - limits.minX;
+    var gHeight = limits.maxY - limits.minY;
 
-    var scale = Math.min(scaleX, scaleY);
-    var x = 0 - limits.minX * scale;
-    var y = 0 - limits.minY * scale;
+    var scaleX = width / gWidth;
+    var scaleY = height / gHeight;
+
+    var scale, x, y;
+    if (scaleX < scaleY) {
+      scale = scaleX;
+      x = 0 - limits.minX * scale;
+      y = 0 - limits.minY * scale + (height-(gHeight*scale))/2;
+    } else {
+      scale = scaleY;
+      x = 0 - limits.minX * scale + (width-(gWidth*scale))/2;
+      y = 0 - limits.minY * scale;
+    }
 
     return {
       x: x,
